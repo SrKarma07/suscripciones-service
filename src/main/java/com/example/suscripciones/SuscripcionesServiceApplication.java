079b84a1c56d05e6ed8dcc219206191cb1ca35d2
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2025-04-02 23:24:24 -0500

    Initial commit

---
+++ src/main/java/com/example/suscripciones/SuscripcionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.suscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscripcionesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuscripcionesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suscripciones/SuscripcionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscripcionesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
